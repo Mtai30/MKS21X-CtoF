a98f4bd2544849e455ed9ef0415650ab8f4c5c87
mtai30 <rqbrawl@gmail.com>
2021-09-23 21:25:24 -0400

    initialized CtoFTester.java

--- CtoFTester.java
+++ CtoFTester.java
@@ -1,7 +1,7 @@
 
 import java.util.Scanner;
 public class CtoFTester{
-      public static void main(String[] args){
+      public static void ctof(String[] args){
           double celsius,fahrenheit;
           Scanner in = new Scanner(System.in);
 
@@ -9,6 +9,17 @@ public class CtoFTester{
           celsius = in.nextDouble();
 
           fahrenheit = (celsius * 1.8) + 32;
+          System.out.print (fahrenheit);
+      }
+
+      public static void ftoc(String[] args){
+          double celsius,fahrenheit;
+          Scanner in = new Scanner(System.in);
+
+          System.out.print("Exactly how many fahrenheit?");
+          fahrenheit = in.nextDouble();
+
+          celsius = (fahrenheit - 32) * (5/9);
           System.out.print (fahrenheit);
       }
 }
