4e1ecd97779f48a9c39d704149283ce976e8175c
mtai30 <rqbrawl@gmail.com>
2021-09-23 21:20:38 -0400

    initialized CtoFTester.java

--- CtoFTester.java
+++ CtoFTester.java
@@ -1,3 +1,14 @@
+
+import java.util.Scanner;
 public class CtoFTester{
-  
+      public static void main(String[] args){
+          double celsius,fahrenheit;
+          Scanner in = new Scanner(System.in);
+
+          System.out.print("Exactly how many celsius?");
+          celsius = in.nextDouble();
+
+          fahrenheit = (celsius * 1.8) + 32;
+          System.out.print (fahrenheit);
+      }
 }
