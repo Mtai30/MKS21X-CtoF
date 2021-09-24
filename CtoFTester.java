91ce2269e2c1866fa56ef74a989b5da67b4d82c3
mtai30 <rqbrawl@gmail.com>
2021-09-23 23:28:37 -0400

    CtoFTester.java

--- CtoFTester.java
+++ CtoFTester.java
@@ -1,25 +1,26 @@
-
+/* The paramenters should be named celsius and fahrenheit for clarity purposes.
+The type should floating-point, because temperature isn't guaranteed to be an integer.
+The functions should return a floating-point because it's likely that it won't convert to a whole number.
+*/
 import java.util.Scanner;
 public class CtoFTester{
-      public static void ctof(String[] args){
-          double celsius,fahrenheit;
-          Scanner in = new Scanner(System.in);
+  public static void main(String[] args) {
+    System.out.println(celsiustofahrenheit(0));
+    System.out.println(fahrenheittocelsius(0));
+  }
 
-          System.out.print("Exactly how many celsius?");
-          celsius = in.nextDouble();
 
-          fahrenheit = (celsius * 1.8) + 32;
-          System.out.print (fahrenheit);
-      }
+  public static double celsiustofahrenheit(double fahrenheit){
+    Scanner in = new Scanner(System.in);
+    System.out.print("Exactly how many fahrenheit?");
+    fahrenheit = in.nextDouble();
+    return ((fahrenheit - 32) * 5 / 9);
+  }
 
-      public static void ftoc(String[] args){
-          double celsius,fahrenheit;
-          Scanner in = new Scanner(System.in);
-
-          System.out.print("Exactly how many fahrenheit?");
-          fahrenheit = in.nextDouble();
-
-          celsius = (fahrenheit - 32) * (5/9);
-          System.out.print (fahrenheit);
-      }
+  public static double fahrenheittocelsius(double celsius){
+    Scanner in = new Scanner(System.in);
+    System.out.print("Exactly how many celsius?");
+    celsius = in.nextDouble();
+    return ((celsius * 1.8) + 32);
+  }
 }
